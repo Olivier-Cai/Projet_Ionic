3871c43745917b2b88d373f01fbd1f51e8421dfa
Olivier-Cai <55697235+Olivier-Cai@users.noreply.github.com>
2020-10-23 21:04:13 +0200

    Update Coquillages.js

--- Bateau/screens/Coquillages.js
+++ Bateau/screens/Coquillages.js
@@ -3,56 +3,38 @@ import { View, StyleSheet,Button } from 'react-native';
 import BackgroundImage from '../components/BackgroundImage';
 import Buttons from '../components/Buttons';
 import {produits} from '../constants/produits.js';
-import images from '../constants/images';
+import images from '../constants/Images';
 
 export default function Coquillages(props){
     return (
         <View style={styles.container}>
           <BackgroundImage/>
-          <View style = {styles.title}>
-                    <Button title="home" onPress={()=>props.navigation.navigate('Home')}/>
+          <View>
+                <Button title="home" onPress={()=>props.navigation.navigate('Home')}/>
           </View>
-          <View style = {styles.coquillages} >
+          <View style={{flexDirection:'column'}}>
             {produits.map((value,index)=>{
               if(value.category==1){
-                return <Buttons style = {{alignItems:'center'}}
+                return <Buttons
                         key={index}
                         text={value.name}
                         image = {images.ancre.uri}
-                        screen={"ProductsList"}
+                        screen={"ProductsLists"}
                         navigation={props.navigation}
                         data={value}>
                         </Buttons>  
               }        
-        })}</View>
-        
-        </View> 
-      );
+            })
+            }
+        </View>
+    </View> 
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex : 1,
-      flexDirection : 'column',
-      alignItems: 'stretch',
-      textAlign: 'center',
-      justifyContent: 'center',
-      
-    },
-    title : {
-        flex: 0.07,
-        justifyContent: 'flex-start',
-      },
-    
-    coquillages : {
-      flex : 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      margin:5,
-      backgroundColor:'rgba(52,52,52,0.3)',
-      borderColor:'black',
-      borderWidth:1,
-      },
-
-    
-  });
+        flex : 1,
+        textAlign: 'center',
+        flexDirection: 'column'
+    }
+});
