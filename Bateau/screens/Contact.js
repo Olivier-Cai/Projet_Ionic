bfc816887d9600c5a92830a1ce80910179c460d7
Olivier-Cai <55697235+Olivier-Cai@users.noreply.github.com>
2020-10-23 21:03:56 +0200

    Update Contact.js

--- Bateau/screens/Contact.js
+++ Bateau/screens/Contact.js
@@ -1,5 +1,4 @@
 import BackgroundImage from '../components/BackgroundImage';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Image,StyleSheet, Text, View } from 'react-native';
 
@@ -8,25 +7,27 @@ export default function(props){
    return(
        <View style = {styles.container}>
           <BackgroundImage/>   
-          <Image style = {styles.image} source = {require('../assets/TIG.png') }></Image>
-          <View >
-            <Text style = {styles.title}>Le bateau de Thibault</Text>
+          
+          <View>
+            <Text style = {styles.title}>Le bateau de Thibault</Text> 
           </View>
-          <View >
+          <View style= {{flex:2, alignItems:"center", paddingTop:20}}>
+            <Image style = {styles.image} source = {require('../assets/TIG.png') }></Image>
             <Text style = {styles.telephone}>06.63.99.99.78</Text>
             <Text style = {styles.mail}>lebateaudethilbaul@gmail.com</Text>
-            <Text style = {styles.facebook}>www.facebook.com/lebateaudethibault</Text>   
-            <Text>{"\n"}{"\n"}{"\n"} 
-               Qu{"'"}il est chaud le soleil {"\n"}
-               Quand nous sommes en vacances {"\n"}
-               Y'a d'la joie, des hirondelles  {"\n"}
-               C est le sud de la France {"\n"}
-               Papa bricole au garage {"\n"}
-               Maman lit dans la chaise longue {"\n"}
-               Dans ce joli paysage {"\n"}
-               Moi, je me balade en tongs {"\n"}
-               {"\n"}Que de bonheur! {"\n"}Que du bonheur! 
-            </Text>
+            <Text style = {styles.facebook}>www.facebook.com/lebateaudethibault</Text>
+          </View>
+          <View style= {{flex:2, alignItems:"center", paddingTop:70}}>     
+            <Text style = {styles.text}>Qu'il est chaud le soleil</Text>
+            <Text style = {styles.text}>Quand nous sommes en vacances</Text>
+            <Text style = {styles.text}>Y'a d'la joie, des hirondelles</Text>
+            <Text style = {styles.text}>C'est le sud de la France</Text>
+            <Text style = {styles.text}>Papa bricole au garage</Text>
+            <Text style = {styles.text}>Maman lit dans la chaise longue</Text>
+            <Text style = {styles.text}>Dans ce joli paysage</Text>
+            <Text style = {styles.text}>Moi, je me balade en tongs</Text>
+            <Text style = {styles.text}>{"\n"}Que de bonheur!</Text>
+            <Text style = {styles.text}>Que de bonheur!</Text> 
           </View>
         </View>
    ); 
@@ -35,25 +36,22 @@ export default function(props){
 
 const styles = StyleSheet.create({
     container: {
-      flex : 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      
+        flex : 1,
+        textAlign: 'center',
+        flexDirection: 'column'
     },
 
     image: {
-        position:'absolute',
-        justifyContent:'flex-start',
-        top : 150,
-        width : 200,
-        height:250,
-      },
+        width : 150,
+        height:200,
+    },
 
-      button: {
+    button: {
         flex : 1,
         flexDirection:'row',
         margin : 2,
     },
+
     title : {
         flex: 1,
         paddingTop: 30,
@@ -63,40 +61,40 @@ const styles = StyleSheet.create({
         fontFamily: 'Snell Roundhand',
         fontWeight: 'bold',
         fontStyle: 'italic'
-      },
-      description : {
+    },
+
+    description : {
         flex : 1,
         marginTop: 50,
         fontFamily: 'Noteworthy',
         fontWeight: 'bold',
         fontSize: 12,
         fontStyle: 'italic'
-      },
-      telephone : {
+    },
+
+    telephone : {
         flex : 1,
         fontSize:12,
         fontFamily: 'Noteworthy'
-      },
-      mail : {
+    },
+
+    mail : {
         flex : 1,
         fontSize:12,
         fontFamily: 'Noteworthy',
         fontStyle: 'italic'
-      },
-      facebook : {
+    },
+
+    facebook : {
         flex : 1,
         fontSize:12,
         fontFamily: 'Noteworthy',
         fontStyle: 'italic'
-      },
-      row : {
-        flex : 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection:"row",
-        margin:2,
-        backgroundColor:'rgba(52,52,52,0.3)',
-        borderColor:'black',
-        borderWidth:1,
-        },
+    },
+
+    text: {
+        fontSize:10,
+        fontFamily: 'Noteworthy',
+        alignItems:'center'
+    }
 });
