96d0194c784c7bfa5f04ef6f601d77505227db89
Olivier-Cai <55697235+Olivier-Cai@users.noreply.github.com>
2020-10-23 21:00:30 +0200

    Update and rename bateau.js to Bateaux.js

---
+++ Bateau/constants/Bateaux.js
@@ -0,0 +1,37 @@
+export const bateaux =[
+    {
+    name : "De la Brise",
+    image : require("../assets/deLaBrise.png"),
+    image_icon : require("../assets/deLaBrise_icon.png"),
+    },
+
+    {
+    name : "Saphir",
+    image : require("../assets/saphir.png"),
+    image_icon : require("../assets/saphir_icon.png"),
+    },
+
+    {
+    name : "Gast Micher",
+    image : require("../assets/gastMicher.png"),
+    image_icon : require("../assets/gastMicher_icon.png"),
+    },
+
+    {
+    name : "Aquillon",
+    image : require("../assets/aquilon.png"),
+    image_icon : require("../assets/aquilon_icon.png"),
+    },
+
+    {
+    name : "Contact",
+    image : require("../assets/ancre.png"),
+    image_icon : require("../assets/ancre.png"),
+    },
+
+    {
+    name : "Contact",
+    image : require("../assets/ancre.png"),
+    image_icon : require("../assets/ancre.png"),
+    },
+]
