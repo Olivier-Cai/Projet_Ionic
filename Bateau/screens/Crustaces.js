15a4b813d220b1f8a106349c480d1e24f4d991a9
Olivier-Cai <55697235+Olivier-Cai@users.noreply.github.com>
2020-10-23 21:04:44 +0200

    Update Crustaces.js

--- Bateau/screens/Crustaces.js
+++ Bateau/screens/Crustaces.js
@@ -3,55 +3,38 @@ import { View, StyleSheet,Button } from 'react-native';
 import BackgroundImage from '../components/BackgroundImage';
 import Buttons from '../components/Buttons';
 import {produits} from '../constants/produits.js';
-import images from '../constants/images';
+import images from '../constants/Images';
 
 export default function Crustaces(props){
     return (
         <View style={styles.container}>
-          <BackgroundImage/>
-          <View style = {styles.title}>
-                    <Button title="home" onPress={()=>props.navigation.navigate('Home')}/>
-          </View>
-          <View style = {styles.crustaces} >
-            {produits.map((value,index)=>{
-              if(value.category==2){
-                return <Buttons style = {{alignItems:'center'}}
-                        key={index}
-                        text={value.name}
-                        image = {images.tourteau.uri}
-                        screen={"ProductsList"}
-                        navigation={props.navigation}
-                        data={value}>
+            <BackgroundImage/>
+            <View>
+                <Button title="home" onPress={()=>props.navigation.navigate('Home')}/>
+            </View>
+            <View style={{flexDirection:'column'}}>
+                {produits.map((value,index)=>{
+                    if(value.category==2){
+                        return <Buttons
+                            key={index}
+                            text={value.name}
+                            image = {images.ancre.uri}
+                            screen={"ProductsLists"}
+                            navigation={props.navigation}
+                            data={value}>
                         </Buttons>  
-              }        
-        })}</View>
-        
+                    }        
+                })
+                }
+            </View>
         </View> 
       );
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex : 1,
-      flexDirection : 'column',
-      alignItems: 'stretch',
-      textAlign: 'center',
-      justifyContent: 'center',
-      
-    },
-    title : {
-        flex: 0.07,
-        justifyContent: 'flex-start',
-      },
-    crustaces : {
-      flex : 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      margin:5,
-      backgroundColor:'rgba(52,52,52,0.3)',
-      borderColor:'black',
-      borderWidth:1,
-      },
-
-    
-  });
+        flex : 1,
+        textAlign: 'center',
+        flexDirection: 'column'
+    }
+});
